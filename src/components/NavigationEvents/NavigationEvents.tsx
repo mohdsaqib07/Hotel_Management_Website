d247749c0e96439b24e1298110d834a62c6d26ec
Saqib <mohdsaqib148183@gmail.com>
2024-02-29 21:20:54 +0530

    stopped the top loading bar to appear on the initial render

--- src/components/NavigationEvents/NavigationEvents.tsx
+++ src/components/NavigationEvents/NavigationEvents.tsx
@@ -5,17 +5,25 @@ import LoadingBar from 'react-top-loading-bar'
 import ThemeContext from '@/context/themeContext'
 export function NavigationEvents() {
   const [progress,setProgress] = useState(0)
+  const [hasRendoredFirst,setHasRendoredFirst] = useState(false);
   const pathname = usePathname()
   const searchParams = useSearchParams()
   const {darkTheme} = useContext(ThemeContext)
-
+  
   const color = darkTheme ? '#e3e3e3' : '#026057'
  
   useEffect(() => {
-     setProgress((prevProgress)=>prevProgress + 40)
-     setTimeout(()=>{
+     if(hasRendoredFirst){
+      setProgress((prevProgress)=>prevProgress + 40)
+      setTimeout(()=>{
       setProgress(100);
      },500)
+     }
+
+     if(!hasRendoredFirst){
+      setHasRendoredFirst(true);
+     }
+     
   
     // You can now use the current URL
     // ...
